acde98dbfdbe3775ffe488e6edd55669ecb37218
k-motwani <40220212+k-motwani@users.noreply.github.com>
2022-09-11 18:05:05 -0400

    add argument to allow customization of sce assayType

--- R/gsexperiment.R
+++ R/gsexperiment.R
@@ -1,21 +1,21 @@
 #' @importClassesFrom SingleCellExperiment SingleCellExperiment
 #' @export
 setClass("gsexperiment", contains = c("SingleCellExperiment"))
-         
+
 #' @importFrom S4Vectors DataFrame SimpleList
 #' @importFrom SingleCellExperiment SingleCellExperiment
 #' @export
 gsexperiment <- function(gs, pop = "root"){
-  leaf <- gs_get_leaf_nodes(gs, pop, path = "auto", showHidden = FALSE)
+  leaf <- gs_get_leaf_nodes(gs, pop, path = "auto", assayType = "intensity", showHidden = FALSE)
   sns <- sampleNames(gs)
   selist <-  lapply(sns, function(sn){
     gh <- gs[[sn]]
     cf <- gh_pop_get_data(gh, pop)
     ca <- CytoArray(cf)
-    
+
     #rowData
     pd <- pData(parameters(cf))
-    
+
     ## somehow can't coerse df to DF
     # pd$name <- as.character(pd$name)
     # pd$desc <- as.character(pd$desc)
@@ -28,7 +28,8 @@ gsexperiment <- function(gs, pop = "root"){
                     , row.names = pd$name #rownames(pd)
                     )
     #colData
-        #label each cell with leaf nodes
+
+    #label each cell with leaf nodes
     ncell <- nrow(cf)
     idx.ancestor <- gh_pop_get_indices(gh, pop)
     idx <- rep(F, ncell)
@@ -57,16 +58,16 @@ gsexperiment <- function(gs, pop = "root"){
   #cbind samples
   se <- do.call(cbind, selist)
   as(se, "gsexperiment")
-  
+
 }
 
 setMethod("GatingSet",c("SingleCellExperiment"),function(x, ...){
-  
-  sce_to_gs(x, ...)  
+
+  sce_to_gs(x, ...)
 })
 
 #' convert SingleCellExperiment to a GatingSet
-#' 
+#'
 #' note that only selected assay and rowData columns will be extracted into the GatingSet
 #' @param sce SingleCellExperiment object
 #' @param assay_type select assay to use
@@ -79,7 +80,7 @@ sce_to_gs <- function(sce
                       , sample = "sample" # the column from colData to represent sample ID
                       , channel = NA # the column from rowData to represent channels
                       , marker = NA # the column from rowData to represent markers
-                      , ...) 
+                      , ...)
 {
   if(!assay_type %in% assayNames(sce))
     stop("assay_type: ", assay_type, " doesn't exist in sce object!")
@@ -89,34 +90,35 @@ sce_to_gs <- function(sce
     chnls <- rownames(rd)
   else
     chnls <- rd[[channel]]
-  
+
   mkrs <- rd[[marker]]
   names(mkrs) <- chnls
-  
+
   #split sce by sample col
   sn_vec <- colData(sce)[[sample]]
   cflist <- sapply(as.character(unique(sn_vec)), function(sn){
                       sce1 <- sce[, sn_vec == sn]
                       #convert assay into cytoframes
-                      mat <- assays(sce1)[[assay_type]]  
+                      mat <- assays(sce1)[[assay_type]]
                       mat <- t(mat)
                       mat <- as.matrix(mat)
                       fr <- flowFrame(mat)
                       markernames(fr) <- mkrs
                       cf <- flowFrame_to_cytoframe(fr)
-                      
+
                       #hack to store cell id in R slot
                       # cf@description[["cellid"]] <- rownames(mat)
                       #TODO:store cell id at cytolib
                       cf
                     })
-  
+
   cs <- cytoset(cflist)
   GatingSet(cs)
-  
-} 
+
+}
 
 gs_get_cell_pop_labels <- function(gs){
+
 }
 
 #' @importFrom dplyr select
